67e5f23f5131025479e6b93a5cecd6bbb3629586
DanielaOrschanski <dorschanski@gmail.com>
2024-08-14 10:01:57 -0300

    Actualizacion

--- R/plot_cnv.R
+++ R/plot_cnv.R
@@ -10,207 +10,175 @@
 #' @import ggtext
 #' @import dplyr
 #' @import showtext
+#' @import tidyr
 
-plot_cnv <- function(CNV_calls) {
+plot_cnv <- function(all.exons, Nspan = 0.5) {
 
-  data("exons.hg19")
-  cnv <- subset(CNV_calls, select = c("start", "end", "name.gene", "type"))
-  colnames(cnv)[3] <- "name"
-  cnv$col <- ifelse(cnv$type == "duplication", "green", "red")
-  bed$col <- "black"
+  library(ggplot2)
+  library(dplyr)
 
-  todo <- rbind(bed[, 2:5], cnv[, c(1, 2, 3, 5)])
-  todo <- todo[order(todo$start), ]
-  todo$yname <- ifelse(todo$col == "black", ifelse(seq_along(todo$col) %% 2 == 0, 0.25, -0.25), ifelse(todo$col == "green", 0.75, -0.75))
-  todo$y <- ifelse(todo$col == "black", 0, ifelse(todo$col == "green", 1, -1))
-
-
-  #Ordenado segun start:
-  gene_order <- unique(todo[order(todo$start), ]$name)
-  todo$name <- factor(todo$name, levels = gene_order)
-  #todo$col <- factor(todo$col)
-
-  #Delete the rows which names are duplicated but keeping the one that is coloured:
-  todo <- ungroup(filter(group_by(todo, name), !(n() > 1 & col == "black")))
-
-  showtext_auto()
-  graph <- ggplot(todo, aes(x = start, y = name)) +
-    geom_segment(aes(xend = end, yend = name), color = todo$col,  size = 1.2) +
-    scale_color_manual(values = c("red", "green", "black")) +
-    scale_fill_manual(values = c("red", "green", "black")) +
-    labs(x = "Posición", y = "Genes", color = "Color", fill = "Color") +
-    theme_classic() +
-    theme(axis.text.y = element_markdown(color = todo$col),
-          axis.text.y.right = element_blank(),
-          axis.text.y.left = element_text(vjust = 1, size= 30),
-          axis.title = element_text(size = 50),
-          axis.text = element_text(size = 50),
-          axis.text.x = element_text(size = 50) )
-  graph
-  #plot_cnv<- recordPlot()
-  return(graph)
-}
-
-#' @title Get CNVs annotation.
-#' @description Generates a graph that represents the CNVs detected. The user will be able to
-#' visualize the distribution, sizes, and positions of the variations within the whole mtDNA
-#' and the genes that are affected by them.
-#' @param chr dataframe that contains the information about the CNVs detected.
-#' @param countThreshold is a number
-#' @return lckerm wdkme
-#' @export
-#'
-#x <- all.exons
-#chr <- 1
-GetCNVsAnnotation <- function(chr, x, countThreshold =10){
-  anno <- x@annotations
-  selected <- which(anno$chromosome == chr & (x@test + x@reference) *
-                      x@expected > countThreshold)
-
-  if (length(selected) == 0) {
-    warning("No exon seem to be located in the requested region. It could also be that the read count is too low for these exons? In any case no graph will be plotted.")
-    return(NULL)
-  }
-  anno <- anno[selected, ]
-  anno$expected <- x@expected[selected]
-  anno$freq <- x@test[selected]/(x@reference[selected] +
-                                   x@test[selected])
-  anno$middle <- 0.5 * (anno$start + anno$end)
-  anno$ratio <- anno$freq/anno$expected
-  anno$test <- x@test[selected]
-  anno$reference <- x@reference[selected]
+  x <- all.exons
+  anno <- all.exons@annotations
+  anno$expected <- x@expected
+  anno$freq <- x@test/(x@reference + x@test)
+  anno$mitad <- 0.5 * (anno$start + anno$end)
+  anno$reads.ratio <- anno$freq/anno$expected
+  anno$test <- x@test
+  anno$reference <- x@reference
   anno$total.counts <- anno$test + anno$reference
-
-  if (length(x@phi) == 1) {
-    anno$phi <- x@phi
-  } else {
-    anno$phi <- x@phi[selected]
-  }
-  anno <- cbind(anno,plyr::ldply(1:nrow(anno), function(i){
-    c(my.min.norm.prop=qbetabinom(p = 0.025, size = anno$total.counts[i],
-                                  phi = anno$phi[i], prob = anno$expected[i]),
-      my.max.norm.prop=qbetabinom(p = 0.975, size = anno$total.counts[i],
-                                  phi = anno$phi[i], prob = anno$expected[i]) )
-  }))
-
-  anno$my.min.norm.prop <- anno$my.min.norm/anno$total.counts
-  anno$my.max.norm.prop <- anno$my.max.norm/anno$total.counts
-  CNVsret <- subset(x@CNV.calls, chromosome == chr)
-  attr(CNVsret,"BamHeader") <- attr(x@CNV.calls,"BamHeader")
-  return(list(Exons=anno, CNVs=CNVsret))
-}
+  colnames(anno)[1] <- "Exons"
 
 
-GetAllExons <- function(cnvCalls = all.exons, countThreshold =10){
-  #chrs <- unique(cnvCalls$CNVs@CNV.calls$chromosome)
-  chrs <- unique(cnvCalls@CNV.calls$chromosome)
-  chr.exons <- bplapply(chrs, GetCNVsAnnotation, x=cnvCalls, BPPARAM = bpparam())
-  names(chr.exons) <- chrs
-  return(invisible(chr.exons))
-}
+  expanded_df <- anno
 
-.PlotCNVchromosome <- function(annot = all.exons, thLength=500, geneTable){
-  ##assume chrome.bed
-  # annot <- chr19
-  # thLength <- 500
-  # annot <- CHRi
+  expanded_df$color <- ifelse(expanded_df$reads.ratio > 1, "duplication", "deletion")
+  expanded_df$color <- ifelse(expanded_df$reads.ratio == 1, "no CNV", expanded_df$color)
 
-  #bh <- attr(annot$CNVs,"BamHeader")
-  #bh <- attr(annot,"BamHeader")
 
-  #data(Centromere)
+  # Agrego el maximo anterior a cada valor de start, end y mitad para que queden ordenados por cromosoma ------------------------------
+  cromosomas <- 1:22
+  expanded_df$globalpos <- NA
+  str(expanded_df)
+  expanded_df$globalpos[which(expanded_df$chromosome == cromosomas[1])] <- expanded_df$mitad[expanded_df$chromosome == cromosomas[1]]
+  expanded_df$globalstart <- NA
+  expanded_df$globalstart[which(expanded_df$chromosome == cromosomas[1])] <- expanded_df$start[expanded_df$chromosome == cromosomas[1]]
+  expanded_df$globalend <- NA
+  expanded_df$globalend[which(expanded_df$chromosome == cromosomas[1])] <- expanded_df$end[expanded_df$chromosome == cromosomas[1]]
+
   load("/media/16TBDisk/Daniela/OMICsdo/data/Centromere.RData")
+  c <- Centromere$hg19
+  c$global_left <- NA
+  c$global_right <- NA
+  c$global_left[which(c$chr == cromosomas[1])] <- c$left[which(c$chr == cromosomas[1])]
+  c$global_right[which(c$chr == cromosomas[1])] <- c$right[which(c$chr == cromosomas[1])]
 
-  Genome <- "hg19"
+  i=2
+  for(i in 2:22) {
+    print(cromosomas[i])
+    max_anterior <-  max(expanded_df$globalpos[expanded_df$chromosome == cromosomas[i-1]])
+    expanded_df$globalpos[which(expanded_df$chromosome == cromosomas[i])] <- expanded_df$mitad[expanded_df$chromosome == cromosomas[i]] + max_anterior
+    expanded_df$globalstart[which(expanded_df$chromosome == cromosomas[i])] <- expanded_df$start[expanded_df$chromosome == cromosomas[i]] + max_anterior
+    expanded_df$globalend[which(expanded_df$chromosome == cromosomas[i])] <- expanded_df$end[expanded_df$chromosome == cromosomas[i]] + max_anterior
 
-  #if(!is.null(bh$GenomeDBversion)){
-  #  Genome <- ifelse(stringr::str_detect(bh$GenomeDBversion,"GRCh38"),"GRCh38","hg19")
-  #}else{
-  #  Genome <- ifelse(!any(stringr::str_detect(bh$Code,"hg19")),"GRCh38","hg19")
-  #}
+    c$global_left[which(c$chr == cromosomas[i])] <- c$left[which(c$chr == cromosomas[i])]  + max_anterior
+    c$global_right[which(c$chr == cromosomas[i])] <- c$right[which(c$chr == cromosomas[i])]  + max_anterior
 
-
-  annot <- GetCNVsAnnotation(chr = 1, x = all.exons)
-  CNVs <- annot$CNVs
-  CNVs$width <- (CNVs$end-CNVs$start)
-  chr <- stringr::str_replace_all(annot$Exons$chromosome[1],"chr","Chr")
-  CNVs <- subset(CNVs, width > thLength)
-
-  annot <- annot$Exons
-  ordm <- order(annot$middle)
-  puntos <- data.frame(x=annot$middle[ordm], y=annot$ratio[ordm])
-  puntos$colscale <- puntos$y
-  puntos$colscale[puntos$colscale>2] <- 2
-  # puntos$Type <- NA
-  # puntos$Type[puntos$y > annot$my.max.norm.prop] <- "Gain"
-  # puntos$Type[puntos$y < annot$my.min.norm.prop] <- "Loss"
-  puntos$ls <- annot$my.max.norm.prop[ordm]/annot$expected[ordm]
-  puntos$li <- annot$my.min.norm.prop[ordm]/annot$expected[ordm]
-  # lsup <- loess(ls~x,puntos, span = 0.3)
-  # linf <- loess(li ~x, puntos, span = 0.3)
-  # df.l<- data.frame(x=puntos$x,y=lsup$fitted)
-  # df.li<- data.frame(x=puntos$x,y=linf$fitted)
-  chr <- unique(annot$chromosome)
-  if (nrow(subset(puntos,x < Centromere[[Genome]][Centromere[[Genome]]$chr==chr,]$left))>0) {
-    lp <- loess(y~x,subset(puntos,x < Centromere[[Genome]][Centromere[[Genome]]$chr==chr,]$left), span = 0.5)
-  } else {
-    lp<-data.frame(x=puntos$x[1],y=puntos$y[1],fitted=puntos$x[1])
+    i <- i+1
   }
 
+  str(expanded_df$chromosome)
 
-  lq <- loess(y~x,subset(puntos,x > Centromere[[Genome]][Centromere[[Genome]]$chr==chr,]$left), span = 0.5)
 
-  p <- ggplot(puntos, aes(x,y,colour=colscale)) + geom_point(size=0.5) +
-    scale_color_stepsn(colours = c("red", "white",  "blue"),breaks = c(  0.9, 1.1 ) )+
-    geom_line(aes(x=x,y=y),data=data.frame(x=lp$x,y=lp$fitted),colour="red")+
-    geom_line(aes(x=x,y=y),data=data.frame(x=lq$x,y=lq$fitted),colour="red")+
-    theme(axis.text.x = element_text(angle=45, vjust=.5, hjust=1, size=0.5)) +
-    ylim(-.1,3.5) + geom_line(aes(x=x,y=y),data=data.frame(x=lp$x,y=lp$fitted),colour="red")+
-    geom_line(aes(x=x,y=y),data=data.frame(x=lq$x,y=lq$fitted),colour="red")+
-    geom_hline(yintercept = 1)
+  expanded_df <- expanded_df %>%
+    mutate(globalpos = as.numeric(globalpos))
 
-  # p <- ggplot(puntos, aes(x,y,colour=colscale)) + geom_point(size=0.5) +
-  #   scale_color_stepsn(colours = c("red", "white",  "blue"),breaks = c(  0.9, 1.1 ) ) +
-  #   geom_smooth(method="loess",span=0.1, se=F,col="black", size=0.5,linetype = "dashed") +
-  #   # geom_smooth(data = puntos,  method="loess",span=0.1, se=F, aes(x=x,y=ls),col="aquamarine4", size=0.5,linetype = "dashed") +
-  # geom_smooth(data = puntos,  method="loess",span=0.1, se=F, aes(x,y=li),col="aquamarine4", size=0.5,linetype = "dashed") +
-  # theme(axis.text.x = element_text(angle=45, vjust=.5, hjust=1, size=0.5)) + ylim(-.1,3.5) + geom_hline(yintercept = 1)
+  #Extraigo las posiciones máximas de start y end entre los cromosomas para poner las lineas verticales ---------------------------
+  max_positions <- data.frame(
+    chromosome = unique(expanded_df$chromosome),
+    max_globalpos = sapply(unique(expanded_df$chromosome), function(x) {
+      max(expanded_df$globalpos[expanded_df$chromosome == x], na.rm = TRUE)
+    }),
+    max_globalend = sapply(unique(expanded_df$chromosome), function(x) {
+      max(expanded_df$globalend[expanded_df$chromosome == x], na.rm = TRUE)
+    }),
+    min_globalstart = sapply(unique(expanded_df$chromosome), function(x) {
+      min(expanded_df$globalstart[expanded_df$chromosome == x], na.rm = TRUE)
+    })
+  )
 
-  # geom_vline(xintercept = chrom.bed[chr,"mid.point"], linetype = "dashed" ) + xlab("Chr. position") + ylab("log ratio")
-  if(nrow(CNVs)>0){
-    if(any(CNVs$reads.ratio>1)){
-      p <- p + geom_segment(data=subset(CNVs,reads.ratio>1), aes(x = start, y = reads.ratio, xend = end, yend = reads.ratio), col="blue", size=2)
+
+  # Genero una línea que marque el promedio de los ratios por cada brazo de cada cromosoma --------------------------------------------
+  medias_p <- data.frame(chr = 1:22, media_p = 0, start = 0, end = 0)
+  medias_q <- data.frame(chr = 1:22, media_q = 0, start = 0, end = 0)
+  list_loes_p <- list()
+  list_loes_q <- list()
+
+  for (i in 1:22) {
+    print(i)
+    expanded_chr <- expanded_df[which(expanded_df$chromosome == i),]
+
+    if (any(is.na(expanded_chr$reads.ratio))) {
+      expanded_chr <- expanded_chr[-which(is.na(expanded_chr$reads.ratio)),]
     }
-    if(any(CNVs$reads.ratio<1)){
-      p <- p + geom_segment(data=subset(CNVs,reads.ratio<1), aes(x = start, y = reads.ratio, xend = end, yend = reads.ratio), col="red", size=2)
+
+    centromere_p <- c$right[which(c$chr == i)]
+
+    #chr13 <- bed_axel[which(bed_axel$chromosome == i),]
+    #chr13 <- exons.hg19[which(exons.hg19$chromosome == i),]
+    #any(chr13$start < centromere_p)
+
+    #Valores medios -----------------------------------------------------------------------------
+    mean_p <- mean(expanded_chr$reads.ratio[which(expanded_chr$mitad < centromere_p)])
+    mean_q <- mean(expanded_chr$reads.ratio[which(expanded_chr$mitad > centromere_p)])
+
+    medias_p[i,"chr"] <- i
+    medias_p[i, "media_p"] <- mean_p
+    medias_p[i, "start"] <- min(expanded_chr$globalstart)
+    medias_p[i, "end"] <- c$global_left[which(c$chr == i)]
+
+    medias_q[i,"chr"] <- i
+    medias_q[i, "media_q"] <- mean_q
+    medias_q[i, "start"] <- c$global_left[which(c$chr == i)]
+    medias_q[i, "end"] <- max(expanded_chr$globalend)
+
+    #loes ----------------------------------------------------
+    # Datos para el brazo p
+    p_arm <- expanded_chr[which(expanded_chr$mitad < centromere_p), ]
+    if(nrow(p_arm) != 0) {
+      loess_p <- loess(reads.ratio ~ globalpos, data = p_arm, span = Nspan)
+      loes_p_df <- data.frame(x= loess_p$x, fitted = loess_p$fitted)
+    } else {
+      loes_p_df <- data.frame()
+    }
+
+    # Datos para el brazo q
+    q_arm <- expanded_chr[which(expanded_chr$mitad > centromere_p), ]
+    loess_q <- loess(reads.ratio ~ globalpos, data = q_arm, span = Nspan)
+    loes_q_df <- data.frame(x= loess_q$x, fitted = loess_q$fitted)
+
+    list_loes_p[[i]] <- loes_p_df
+    list_loes_q[[i]] <- loes_q_df
+  }
+
+  medias_p[is.na(medias_p)] <- 1
+  medias_q[is.na(medias_q)] <- 1
+
+
+  c$chr <- paste("chr", c$chr, sep=" ")
+
+  nas <- expanded_df[which(is.na(expanded_df$reads.ratio)),]
+  expanded_df$reads.ratio[which(is.na(expanded_df$reads.ratio))] <- 1
+  expanded_df$color[which(expanded_df$reads.ratio == 1)] <- "no CNV"
+  expanded_df$colorcnvs <- ifelse(expanded_df$Exons %in% exons_cnvs$Exons, "CNV", "no CNV")
+  length(which(expanded_df$colorcnvs == "CNV"))
+
+  g <- ggplot(expanded_df, aes(x = globalpos, y = (reads.ratio), color = color)) +
+    geom_point() +
+    geom_segment(data = medias_p, aes(x = start, y = (media_p), xend = end, yend = (media_p)), col="orange", size=2) +
+    geom_segment(data = medias_q, aes(x = start, y = (media_q), xend = end, yend = (media_q)), col="orange", size=2) +
+    scale_color_manual(values = c("purple", "lightblue", "black")) +
+    theme_minimal() +
+    labs(x = "Posición dentro del cromosoma (mitad)", y = "Reads Ratio", title = "Distribución de reads.ratio por cromosoma") +
+    #geom_vline(data = max_positions, aes(xintercept = min_globalstart), color = "yellow") +
+    geom_vline(xintercept = max_positions$min_globalstart[1], color = "black") +
+    geom_vline(data = max_positions, aes(xintercept = max_globalend), color = "black") +
+    geom_hline(yintercept = 1, color = "black", linetype = "solid", size = 1) +
+    scale_x_continuous(breaks = c$global_left, labels = c$chr) +
+    geom_vline(data = c, aes(xintercept = global_left), linetype = "dotted", color = "black") +
+    scale_y_continuous(breaks = seq(-5, 5, by = 0.5)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 12))
+  print(g)
+
+  g2 <- g
+  for (i in 1:22) {
+    g2 <- g2 + geom_line(aes(x = globalpos, y = fitted), data = list_loes_q[[i]], colour = "red")
+
+    if (nrow(list_loes_p[[i]]) != 0) {
+      g2 <- g2 + geom_line(aes(x = globalpos, y = fitted), data = list_loes_p[[i]], colour = "red")
     }
   }
-  if(missing(geneTable) == FALSE){
-    chr <- unique(annot$chromosome)
-    genes <- subset(geneTable, chromosome == chr)
-    if(nrow(genes)>0){
-      exons <- plyr::ldply(genes$Gene,function(x){
-        ret <- subset(annot, stringr::str_detect(name, paste0("^",x,"_")))
-        if(nrow(ret)!=0){
-          ret$gene <- x
-          return(ret)
-        }
 
-      })
-      if(nrow(exons)>0){
-        require(dplyr)
-        require(ggrepel)
-        labels <- exons %>% group_by(gene) %>% summarise(x=min(middle),y=max(ratio))
-        p <- p + geom_point(data=exons, mapping=aes(x=middle,y=ratio, shape=gene), colour = "black") +
-          geom_text_repel(data=as.data.frame(labels), aes(x,y=3, label=gene, size=0.6), hjust=0, vjust=0,angle=90,colour="black")
-      }
+  print(g2)
+  return(g2)
 
-    }
-
-  }
-  p <- p + labs(title = paste0("Chr ",unique(annot$chromosome)),x="base position",y="Ratio") +
-    theme(legend.position = "none",plot.title = element_text(margin = margin(t=40,b=-30)))
-  print(p)
-
-  return(invisible(p))
 }
